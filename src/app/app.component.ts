1200a465afb334adf9808d21f3ad311c5cbb55fb
Paloma González-Ripoll <palgoncan05@iesfidiana.es>
2024-10-29 11:18:26 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { CronoComponent } from './components/crono/crono.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CronoComponent,
+    RouterLink,RouterLinkActive,ButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'helloworld';
+  numero:number = 0;
+
+  constructor(){
+    setInterval(()=>{
+      this.numero++;
+      if (this.numero==10){
+        this.numero=0;
+      }
+    },1000);
+  }
+
+  reseteaSegundos():void {
+    this.numero = 0;
+  }
+
+  hazalgo(event?:any):void{
+    console.log(event);
+  }
 }
---
+++ src/app/components/create-task/create-task.component.ts
@@ -0,0 +1,26 @@
+import { Component, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { task } from '../../model/task';
+
+@Component({
+  selector: 'app-create-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-task.component.html',
+  styleUrl: './create-task.component.css'
+})
+export class CreateTaskComponent {
+  @ViewChild("texto") el_texto: ElementRef | undefined;
+  @Output() taskCreated = new EventEmitter<task>();
+
+  createTask(){
+      if(!this.el_texto) return;
+      let title = this.el_texto?.nativeElement.value.trim();
+      if(!title) return;
+      let timestamp = Date.now();
+      let newNote:task = {id:Math.random()*timestamp, title:title};
+      this.taskCreated.emit(newNote);
+      (this.el_texto.nativeElement as any).value="";
+      return false; //si no devuelve false se envia el formulario y refresca la pagina (no sale la tarea nueva)
+    }
+
+}
