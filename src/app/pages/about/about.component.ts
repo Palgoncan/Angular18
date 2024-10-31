6954885c37506888cb23eaa1686910cd0be2b317
Paloma González-Ripoll <palgoncan05@iesfidiana.es>
2024-10-31 13:48:09 +0100

    cambios

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -8,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  edad = signal(5);
+  precio = computed(()=>{ //DECLARATIVA
+    if(this.edad()<18){
+      return 5;
+    }else if(this.edad()<65){
+      return 10;
+    }else {
+      return 0;
+    }
+  });
 
+  ngOnInit(){
+    setTimeout(()=>{
+      this.edad.set(18);
+    },2000)
+    setTimeout(()=>{
+      this.edad.set(80);
+    },8000)
+  }
 }
